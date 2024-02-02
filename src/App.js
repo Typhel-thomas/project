a02394cdbf5a110855306fe03b1ba89c1de47d05
Typhel-thomas <158449462+Typhel-thomas@users.noreply.github.com>
2024-02-02 13:29:43 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,46 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+    <div>
+      <div className="text">
+        <h1>Director and Movies</h1>
+      </div>
+      <div>
+        <div className="text1">
+          <p1>Director name</p1>
+        </div>
+        <div>
+          <div className="text2">
+            <p1>Atlee</p1>
+          </div>
+          <div>
+            <div className="text3">
+              <p1>Mohan lal</p1>
+            </div>
+            <div>
+              <div className="text4">
+                <p1>movie name</p1>
+              </div>
+              <div>
+                <div className="btn">
+                  <button>Add</button>
+                </div>
+                <div>
+                  <div className="text5">
+                    <p1>James Bond</p1>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    </div>
+  );
+}
+
+export default App;
